6514886f136fc890e7ea3d10aca9924738873907
hamidhayatijozani <hamidhayatijozani@gmail.com>
2026-09-17 23:23:28 +0330

    chore(action-gate): expose security authority primitives

---
+++ action_gate/__init__.py
@@ -0,0 +1,10 @@
+from .security_authority import Authority, AuthorityError, canonical_digest, canonical_json, issue_authority, verify_authority
+
+__all__ = [
+    "Authority",
+    "AuthorityError",
+    "canonical_digest",
+    "canonical_json",
+    "issue_authority",
+    "verify_authority",
+]
